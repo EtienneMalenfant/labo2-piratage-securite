119e26742c3665f4c984a48e6d92f9ed51bb0def
EtienneMalenfant <125925730+EtienneMalenfant@users.noreply.github.com>
2024-09-06 10:04:03 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> hashedPass = new List<string>();
+        loadHashedPasswords(hashedPass);
+        writeHashedPasswords(hashedPass);
+        Console.WriteLine("Conversion termine");
+    }
+
+    static void loadHashedPasswords(List<string> hashedPass)
+    {
+        MD5 hashing = MD5.Create();
+        using (StreamReader file = new StreamReader("input.txt"))
+        {
+            while (!file.EndOfStream)
+            {
+                string line = file.ReadLine();
+                byte[] data = hashing.ComputeHash(Encoding.UTF8.GetBytes(line));
+                hashedPass.Add(BitConverter.ToString(data).Replace("-", "").ToLower());
+            }
+        }
+    }
+
+    static void writeHashedPasswords(List<string> hashedPass)
+    {
+        using (StreamWriter file = new StreamWriter("output.txt"))
+        {
+            foreach (string line in hashedPass)
+            {
+                file.WriteLine(line);
+            }
+        }
+    }
+}
